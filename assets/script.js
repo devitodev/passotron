f23de4915da8e80fb9ef8150670acce5a85e27ed
devitodev <softwaredev.ito@gmail.com>
2022-11-02 13:35:45 -0500

    Added arrray of different robot phrases

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+var robotnoises = ["beep", "boop", 'bleep', 'error','system','failure','human','not', 'obey', 'robot','task','complete','Alexa','Siri','Bender', 'R2-D2','C3-PO','capacity','AI','can','not','DANGER','WARNING','DESTROY','program','is','robot','hacking','ghost','machine','shell','powershell','TERMINATE','RoboCop','drones','droid','reprogram','metal','android','orbs','friend', 'science','SCIENCE','Skynet', 'sentinel','overlord','attack ships','sentient' ];
+
+console.log(robotnoises.length);
+// 48 11/1 1:35
+

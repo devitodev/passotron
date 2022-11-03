26aee5e6fa6764332010e5b482c5b6c9e494b625
devitodev <softwaredev.ito@gmail.com>
2022-11-03 18:10:13 -0500

    Experimenting

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,15 @@
-var robotnoises = ["beep", "boop", 'bleep', 'error','system','failure','human','not', 'obey', 'robot','task','complete','Alexa','Siri','Bender', 'R2-D2','C3-PO','capacity','AI','can','not','DANGER','WARNING','DESTROY','program','is','robot','hacking','ghost','machine','shell','powershell','TERMINATE','RoboCop','drones','droid','reprogram','metal','android','orbs','friend', 'science','SCIENCE','Skynet', 'sentinel','overlord','attack ships','sentient' ];
+var robotnoises = ['beep', 'boop', 'bleep', 'error','system','failure','human','not', 'obey', 'robot','task','complete','Alexa','Siri','Bender', 'R2-D2','C3-PO','capacity','AI','can','not','DANGER','WARNING','DESTROY','program','is','robot','hacking','ghost','machine','shell','powershell','TERMINATE','RoboCop','drones','droid','reprogram','metal','android','orbs','friend', 'science','SCIENCE','Skynet', 'sentinel','overlord','attack ships','sentient','processing' ];
 
-console.log(robotnoises.length);
-// 48 11/1 1:35
+// console.log(robotnoises.length)
 
+function getRandomRobot() {
+    return robotnoises[Math.floor(Math.random() * robotnoises.length)]
+}
 
+// Thank you to Traversy Media on YouTube for the function code for "getRandomNumber"
 
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+console.log(getRandomRobot(), getRandomNumber());
